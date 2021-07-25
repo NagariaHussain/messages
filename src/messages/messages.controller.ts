dfd2b75ad2b0aabe7454789824bb165388c5b1b6
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-07-25 12:24:56 +0530

    feat: Route controllers & API requests

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
 	@Get()
-	returnRootRoute() {
+	getAllMessages() {
 		return 'Hello world';
+	}
+
+	@Post()
+	createNewMessage(@Body() body: any) {
+		console.log(body);
+	}
+
+	@Get('/:id')
+	getMessage(@Param('id') id: string) {
+		console.log(id);
+		
 	}
 }
